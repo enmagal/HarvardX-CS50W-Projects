d6422ad0db82df93d584ea6e6f2d65b1bf981a82
enmagal <enzo.magal@gmail.com>
2024-03-26 21:39:09 +0100

    :construction: Begin Project 1

--- Project 1 - Wiki/wiki_adonis/start/routes.ts
+++ Project 1 - Wiki/wiki_adonis/start/routes.ts
@@ -7,6 +7,33 @@
 |
 */
 
+import { Exception } from '@adonisjs/core/exceptions'
 import router from '@adonisjs/core/services/router'
+import app from '@adonisjs/core/services/app'
+import fs from 'node:fs/promises'
+import { MarkdownFile } from '@dimerapp/markdown'
+import { toHtml } from '@dimerapp/markdown/utils'
 
-router.on('/').render('pages/home')
+router.on('/').render('pages/home').as('home')
+
+router
+  .get('/wiki/:title', async (ctx) => {
+    const url = app.makeURL(`resources/entries/${ctx.params.title}.md`)
+    try {
+      const file = await fs.readFile(url, 'utf8')
+      const md = new MarkdownFile(file)
+
+      await md.process()
+
+      const entry = toHtml(md).contents
+
+      ctx.view.share({ entry, md })
+    } catch (error) {
+      throw new Exception(`Could not find the movie called ${ctx.params.slug}`, {
+        code: 'E_NOT_FOUND',
+        status: 404,
+      })
+    }
+    return ctx.view.render('pages/entry/show')
+  })
+  .as('entries.show')
