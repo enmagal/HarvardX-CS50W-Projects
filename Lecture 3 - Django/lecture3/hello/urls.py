344aa9805af65212d0254fac5371a40fb003401c
enmagal <enzo.magal@gmail.com>
2024-03-26 19:34:02 +0100

    :sparkles: Hello App

--- Lecture 3 - Django/lecture3/hello/urls.py
+++ Lecture 3 - Django/lecture3/hello/urls.py
@@ -1,7 +1,10 @@
-from django.urlpatterns import path
+from django.urls import path
 
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index")
+    path("", views.index, name="index"),
+    path("<str:name>", views.greet, name="greet"),
+    path("enzo", views.enzo, name="enzo"),
+    path("agathe", views.agathe, name="agathe")
 ]
